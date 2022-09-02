22e59cfca5202d54a361cafd4b7deedcae166789
osvaldo <g.osvaldo96@gmail.com>
2022-09-01 22:39:15 -0600

    pages folder files updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Footer from 'components/Footer'
 import Navbar from 'components/Navbar'
+import Home from 'pages/Home'
+import AboutMe from 'pages/AboutMe'
+import Skills from 'pages/Skills'
+import Projects from 'pages/Projects'
+import Contact from 'pages/Contact'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Projects />
+      <Contact />
       <Footer />
     </>
   );
