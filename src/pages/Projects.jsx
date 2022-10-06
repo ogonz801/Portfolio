3b67dd27f20f145ee8514bb17e869b6bbae37a17
osvaldo <g.osvaldo96@gmail.com>
2022-10-05 21:15:30 -0600

    update to projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -17,15 +17,15 @@ const Projects = () => {
           </p>
         </div>
         <div className="grid gap-3 sm:grid-cols-2 md:grid-cols-3">
-          <div className="bg-[#060f1e] container rounded-md flex justify-center items-center mx-auto content-div">
+          <div className="bg-[#060f1e] p-2 container rounded-md flex justify-center items-center mx-auto content-div">
             <div>
               <span className="my-2 opacity-100 flex justify-center items-center text-xl font-bold tracking-wider">
                 Whack-A-Mole
               </span>
-              <p className="text-center">
-                JavaScript game
-                <br></br>
-                created using HTML, CSS, and JavaScript
+              <p className="text-sm text-center">
+                JavaScript base game making use of DOM manipulation.
+                <br/>
+                Built using HTML, CSS, and vanilla JavaScript.
               </p>
               <div className="mt-6 py-3 text-center flex justify-evenly items-center">
                 <a
@@ -43,15 +43,15 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#060f1e] container rounded-md flex justify-center items-center mx-auto content-div">
+          <div className="bg-[#060f1e] p-2 container rounded-md flex justify-center items-center mx-auto content-div">
             <div>
               <span className="my-2 opacity-100 flex justify-center items-center text-xl font-bold tracking-wider">
-                Music Playlist App
+                Crudify
               </span>
-              <p className="text-center">
-                created using Nodejs, Express, EJS, Mongodb, and Mongoose
-                <br></br>
-                deployed using Heroku
+              <p className="text-sm text-center">
+                A music playlist app allowing with full CRUD functionality. Built using Node.js, Mongoose, MongoDB, and EJS.
+                <br/>
+                Utilizing the Spotify OAuth for authentication and authorization.
               </p>
               <div className="py-3 text-center flex justify-evenly items-center">
                 <a
@@ -69,15 +69,15 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#060f1e] container rounded-md flex justify-center items-center mx-auto content-div">
+          <div className="bg-[#060f1e] p-2 container rounded-md flex justify-center items-center mx-auto content-div">
             <div>
               <span className="my-2 opacity-100 flex justify-center items-center text-xl font-bold tracking-wider">
                 Noms On Wheels
               </span>
-              <p className="text-center">
-                Full stack group project
-                <br></br>
-                created using Nodejs, Express, Mongodb, Mongoose, Reactjs
+              <p className="text-sm text-center">
+                A group full stack app that allows users to find food trucks in their area and owners to add their own. I handle the backend and utilized passport local and bcrypt for user authentication and authorization.
+                <br/>
+                Built using Node.js with express framework as the backend, MongoDB as the database, and React.js for the frontend.
               </p>
               <div className="py-3 text-center flex justify-evenly items-center">
                 <a
@@ -95,18 +95,19 @@ const Projects = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#060f1e] container rounded-md flex justify-center items-center mx-auto content-div">
+          <div className="bg-[#060f1e] p-2 container rounded-md flex justify-center items-center mx-auto content-div">
             <div>
               <span className="my-2 opacity-100 flex justify-center items-center text-xl font-bold tracking-wider">
-                Portfolio 
+                Torq 
               </span>
-              <p className="text-center">
-                overtime this portfolio site will go through changes and updates
-                
+              <p className="text-sm text-center">
+                A full stack vehicle maintenance app for users to set reminders for future maintenance.
+                <br/>
+                Built using Django as the backend, Postgresql as the database, and React.js for the frontend.
               </p>
               <div className="py-3 text-center flex justify-evenly items-center">
                 <a
-                  href="https://github.com/ogonz801/Portfolio"
+                  href="https://github.com/ogonz801/Torq"
                   className="m-2 text-[#ccd6f6] text-3xl hover:text-pink-500"
                 >
                   <FaGithub />
